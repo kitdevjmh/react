9b3a996483a6b5244d347c879951ee6b4c193fb4
devjmh <jyoutubh87@gmail.com>
2025-04-17 01:09:21 +0900

    Add files via upload

---
+++ 0410/hooks/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./App"
+import ReactDOM from "react-dom/client";
+// import Counter from './chapter_07/Counter';
+// import Counter2 from './chapter_07/Counter2';
+// import Counter3 from './chapter_07/Counter3';
+// import Counter3 from './chapter_07/Counter4';
+// import Counter6 from './chapter_07/Counter6';
+import Area from './chapter_07/Area';
+
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<Area />);
